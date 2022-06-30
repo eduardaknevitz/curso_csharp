989d60c007cbfeda844f6b6910576c302a108633
Eduarda Knevitz <eduarda.knevitz@quattrus.com>
2022-06-30 14:58:29 -0300

    Teste 2

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,6 +7,7 @@ namespace PrimeiroProjeto //namespace = agrupamento de classes relacionadas
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste!");
         }
     }
 }
